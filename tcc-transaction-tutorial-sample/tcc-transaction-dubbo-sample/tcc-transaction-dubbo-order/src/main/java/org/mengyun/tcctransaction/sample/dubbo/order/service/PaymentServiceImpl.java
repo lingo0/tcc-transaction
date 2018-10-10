b94a61c965ebfd35d620ee27a370c2e515aeff2e
lingo0 <315914415@qq.com>
2018-10-10 20:39:51 +0800

    error1 红包账户冻结成功(try)、资金账户冻结成功(try)，订单操作异常(try)

--- tcc-transaction-tutorial-sample/tcc-transaction-dubbo-sample/tcc-transaction-dubbo-order/src/main/java/org/mengyun/tcctransaction/sample/dubbo/order/service/PaymentServiceImpl.java
+++ tcc-transaction-tutorial-sample/tcc-transaction-dubbo-sample/tcc-transaction-dubbo-order/src/main/java/org/mengyun/tcctransaction/sample/dubbo/order/service/PaymentServiceImpl.java
@@ -47,6 +47,9 @@ public class PaymentServiceImpl {
 
         String result = capitalTradeOrderService.record(buildCapitalTradeOrderDto(order));
         String result2 = redPacketTradeOrderService.record(buildRedPacketTradeOrderDto(order));
+
+        // error1 红包账户冻结成功(try)、资金账户冻结成功(try)，订单操作异常(try)
+        throw new RuntimeException("error1 红包账户冻结成功(try)、资金账户冻结成功(try)，订单操作异常(try)");
     }
 
     public void confirmMakePayment(Order order, BigDecimal redPacketPayAmount, BigDecimal capitalPayAmount) {
